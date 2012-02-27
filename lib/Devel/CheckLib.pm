f8cec26f27e8dc11cea9a41ed8c81281668ef12d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2012-02-27 00:15:43 +0000

    Import of MATTN/Devel-CheckLib-0.97 from CPAN.
    gitpan-cpan-distribution: Devel-CheckLib
    gitpan-cpan-version:      0.97
    gitpan-cpan-path:         MATTN/Devel-CheckLib-0.97.tar.gz
    gitpan-cpan-author:       MATTN
    gitpan-cpan-maturity:     released

--- lib/Devel/CheckLib.pm
+++ lib/Devel/CheckLib.pm
@@ -5,7 +5,7 @@ package Devel::CheckLib;
 use 5.00405; #postfix foreach
 use strict;
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = '0.96';
+$VERSION = '0.97';
 use Config qw(%Config);
 use Text::ParseWords 'quotewords';
 
