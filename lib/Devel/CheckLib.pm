1edf92b261cb091f984606a17a074ba6aad2b050
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2013-04-03 01:14:12 +0000

    Import of MATTN/Devel-CheckLib-0.99 from CPAN.
    gitpan-cpan-distribution: Devel-CheckLib
    gitpan-cpan-version:      0.99
    gitpan-cpan-path:         MATTN/Devel-CheckLib-0.99.tar.gz
    gitpan-cpan-author:       MATTN
    gitpan-cpan-maturity:     released

--- lib/Devel/CheckLib.pm
+++ lib/Devel/CheckLib.pm
@@ -5,7 +5,7 @@ package Devel::CheckLib;
 use 5.00405; #postfix foreach
 use strict;
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = '0.98';
+$VERSION = '0.99';
 use Config qw(%Config);
 use Text::ParseWords 'quotewords';
 
@@ -114,6 +114,11 @@ libraries (each preceded by '-l') and directories (preceded by '-L').
 
 This can also be supplied on the command-line.
 
+=item debug
+
+If true - emit information during processing that can be used for
+debugging.
+
 =back
 
 And libraries are no use without header files, so ...
@@ -202,7 +207,7 @@ sub assert_lib {
     # using special form of split to trim whitespace
     if(defined($args{LIBS})) {
         foreach my $arg (split(' ', $args{LIBS})) {
-            die("LIBS argument badly-formed: $arg\n") unless($arg =~ /^-l/i);
+            die("LIBS argument badly-formed: $arg\n") unless($arg =~ /^-[lLR]/);
             push @{$arg =~ /^-l/ ? \@libs : \@libpaths}, substr($arg, 2);
         }
     }
